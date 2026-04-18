e8c538b8de065ed27788ac2b5d40a0177d9497b7
AndreaBianucci <andreabianucci04@gmail.com>
2026-04-18 09:22:48 +0200

    Update ESP12F.ino
    Possible errors have been fixed.

--- ESP12F.ino
+++ ESP12F.ino
@@ -1,108 +1,57 @@
 #include <SoftwareSerial.h>
-#include <string>
 #include <ESP8266WiFi.h>
-#include <ESP8266HTTPClient.h> 
+#include <ESP8266HTTPClient.h>
 
-const char* ssid = "[name of your personal SSID if you want to connect to your hotspot]";
-const char* password = "[psw of your SSID]";
+const char* ssid = "TUO_SSID";
+const char* password = "TUA_PASSWORD";
 
-const char* ssid2 = "[name of your personal SSID if you want to connect to your Wifi";
-const char* password2 = "[psw of your SSID]";
-
-HTTPClient http;  //Declare an object of class HTTPClient
+SoftwareSerial ss(4, 5); // RX = D2, TX = D1
 WiFiClient client;
-int httpCode;
-String payload;
-int msg;
- 
-SoftwareSerial ss(4, 5); // rx, tx
-
-String lat = "";
-String lng = "";
-int cosa = 0;
+HTTPClient http;
 
 void setup() {
-  ss.begin(9600);
   Serial.begin(9600);
-  Serial.print("Connecting.."); 
-  Serial.print("Connesso");
+  ss.begin(9600);
+  
+  WiFi.begin(ssid, password);
+  Serial.print("Connessione WiFi");
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("\nWiFi Connesso!");
 }
 
 void loop() {
-  
   if (ss.available()) {
     String data = ss.readStringUntil('\n');
-    Serial.print("ok");
-    String latitude = "", longitude = "";
-    char * datoFinale;
+    
     if (data.startsWith("$GPGGA")) {
-      Serial.println("Data: " + data);
+      // Parsing semplice
+      String values[10];
+      int startIndex = 0;
+      for (int i = 0; i < 10; i++) {
+        int endIndex = data.indexOf(',', startIndex);
+        values[i] = data.substring(startIndex, endIndex);
+        startIndex = endIndex + 1;
+      }
 
-      String values[15]; // Array per memorizzare i valori separati
+      String latitude = values[2];  // DDMM.MMMM
+      String longitude = values[4]; // DDDMM.MMMM
+      
+      if (latitude.length() > 0 && WiFi.status() == WL_CONNECTED) {
+        String url = "http://www.trackergps.altervista.org/Esp.php?id=2&latitude=" + latitude + "&longitude=" + longitude;
         
-        int valueIndex = 0;
-        int startIndex = 0;
-        int endIndex = data.indexOf(',');
-  
-        while (endIndex != -1 && valueIndex < 15) {
-          values[valueIndex] = data.substring(startIndex, endIndex);
-          startIndex = endIndex + 1;
-          endIndex = data.indexOf(',', startIndex);
-          valueIndex++;
-        }
-  
-        latitude = values[2];
-        longitude = values[4];
+        http.begin(client, url);
+        int httpCode = http.GET();
         
-        if (latitude != "" && longitude != ""){
-
-            //if (WiFi.status() != WL_CONNECTED){
-                WiFi.begin(ssid, password);
-                Serial.print("Connecting.."); 
-                //int cont = 0;
-                //while (WiFi.status() != WL_CONNECTED && cont<10) {
-                while (WiFi.status() != WL_CONNECTED) {
-                  delay(1000);
-                  Serial.print(".");
-                  //cont++;
-                }
-    
-                //cont = 0;
-                /*Serial.println("PRIMA FAILED");
-                
-                WiFi.begin(ssid2, password2);
-                Serial.print("Connecting.."); 
-                while (WiFi.status() != WL_CONNECTED && cont<10) {
-                  delay(1000);
-                  Serial.print(".");
-                  cont++;
-                }
-
-                Serial.println("SECONDA FAILED");*/
-            //}
-    
-            http.begin(client, "http://www.trackergps.altervista.org/Esp.php?id=2&latitude="+latitude+"&longitude="+longitude+"");  //Specify request destination
-            msg = http.GET();
-            Serial.print(msg);
-            WiFi.disconnect();
+        if (httpCode > 0) {
+          Serial.printf("[HTTP] Codice: %d\n", httpCode);
+        } else {
+          Serial.printf("[HTTP] Errore: %s\n", http.errorToString(httpCode).c_str());
         }
+        http.end();
+      }
     }
-        /*String values[15]; // Array per memorizzare i valori separati
-        
-        int valueIndex = 0;
-        int startIndex = 0;
-        int endIndex = data.indexOf(',');
-  
-        while (endIndex != -1 && valueIndex < 15) {
-          values[valueIndex] = data.substring(startIndex, endIndex);
-          startIndex = endIndex + 1;
-          endIndex = data.indexOf(',', startIndex);
-          valueIndex++;
-        }
-  
-        latitude = values[2];
-        longitude = values[4];*/
-
-        
-    }
+  }
 }
