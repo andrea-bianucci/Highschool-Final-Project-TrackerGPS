0f477108cbc8108693bd043a7ceef388389a73d3
AndreaBianucci <andreabianucci04@gmail.com>
2026-04-18 09:26:47 +0200

    Update ESP12F.ino
    Possible errors have been fixed.

--- ESP12F.ino
+++ ESP12F.ino
@@ -5,52 +5,61 @@
 const char* ssid = "TUO_SSID";
 const char* password = "TUA_PASSWORD";
 
-SoftwareSerial ss(4, 5); // RX = D2, TX = D1
+SoftwareSerial ss(4, 5); // RX=D2, TX=D1
 WiFiClient client;
 HTTPClient http;
+
+unsigned long lastTime = 0;
+unsigned long timerDelay = 10000; // Invia i dati ogni 10 secondi
 
 void setup() {
   Serial.begin(9600);
   ss.begin(9600);
   
+  WiFi.mode(WIFI_STA); // Imposta come client
   WiFi.begin(ssid, password);
-  Serial.print("Connessione WiFi");
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.print(".");
-  }
-  Serial.println("\nWiFi Connesso!");
+  
+  Serial.print("Connessione in corso...");
+  // Nota: non mettiamo un loop infinito qui se vuoi che il codice parta subito, 
+  // ma è meglio aspettare il primo aggancio.
 }
 
 void loop() {
+  // 1. Leggi SEMPRE il GPS per svuotare il buffer seriale
   if (ss.available()) {
     String data = ss.readStringUntil('\n');
     
     if (data.startsWith("$GPGGA")) {
-      // Parsing semplice
-      String values[10];
-      int startIndex = 0;
-      for (int i = 0; i < 10; i++) {
-        int endIndex = data.indexOf(',', startIndex);
-        values[i] = data.substring(startIndex, endIndex);
-        startIndex = endIndex + 1;
-      }
+      // Parsing veloce della stringa
+      int comma2 = data.indexOf(',', 7);
+      int comma3 = data.indexOf(',', comma2 + 1);
+      int comma4 = data.indexOf(',', comma3 + 1);
+      int comma5 = data.indexOf(',', comma4 + 1);
 
-      String latitude = values[2];  // DDMM.MMMM
-      String longitude = values[4]; // DDDMM.MMMM
-      
-      if (latitude.length() > 0 && WiFi.status() == WL_CONNECTED) {
-        String url = "http://www.trackergps.altervista.org/Esp.php?id=2&latitude=" + latitude + "&longitude=" + longitude;
-        
-        http.begin(client, url);
-        int httpCode = http.GET();
-        
-        if (httpCode > 0) {
-          Serial.printf("[HTTP] Codice: %d\n", httpCode);
-        } else {
-          Serial.printf("[HTTP] Errore: %s\n", http.errorToString(httpCode).c_str());
+      String latitude = data.substring(comma2 + 1, comma3);
+      String longitude = data.substring(comma4 + 1, comma5);
+
+      // 2. Invia i dati solo se il timer è scaduto E il WiFi è connesso
+      if ((millis() - lastTime) > timerDelay) {
+        if (WiFi.status() == WL_CONNECTED && latitude.length() > 5) {
+          
+          String url = "http://www.trackergps.altervista.org/Esp.php?id=2&latitude=" + latitude + "&longitude=" + longitude;
+          
+          http.begin(client, url);
+          int httpCode = http.GET(); // Effettua la chiamata
+          
+          if (httpCode > 0) {
+            Serial.println("Dati inviati! Risposta: " + String(httpCode));
+          } else {
+            Serial.println("Errore HTTP: " + http.errorToString(httpCode));
+          }
+          http.end();
+          
+          lastTime = millis(); // Reset del timer
+        } else if (WiFi.status() != WL_CONNECTED) {
+          Serial.println("WiFi disconnesso, riconnessione automatica...");
+          // Non serve fare nulla, l'ESP8266 gestisce la riconnessione da solo se WiFi.begin è stato chiamato
         }
-        http.end();
       }
     }
   }
